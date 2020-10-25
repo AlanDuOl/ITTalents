2132b08953d1d88ad0f1c1db61e3c8d192cb146f
alanduarte90 <alanduarte90@gmail.com>
2020-10-25 18:44:20 +0000

    create(test): add test to logout component

--- EasyTalents/ClientApp/src/api-authorization/logout/logout.component.spec.ts
+++ EasyTalents/ClientApp/src/api-authorization/logout/logout.component.spec.ts
@@ -1,25 +1,44 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthorizeService } from '../authorize.service';
 
-// import { LogoutComponent } from './logout.component';
+import { LogoutComponent } from './logout.component';
 
-// describe('LogoutComponent', () => {
-//   let component: LogoutComponent;
-//   let fixture: ComponentFixture<LogoutComponent>;
+describe('LogoutComponent', () => {
+  let component: LogoutComponent;
+  let fixture: ComponentFixture<LogoutComponent>;
+  let mockAuthService: Partial<AuthorizeService>;
+  let authorizeService: AuthorizeService;
+  let mockActivedRoute: Partial<ActivatedRoute>;
+  let activatedRoute: ActivatedRoute;
+  let mockRouter: Partial<Router>;
+  let router: Router;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ LogoutComponent ]
-//     })
-//     .compileComponents();
-//   }));
+  beforeEach(async(() => {
+    mockAuthService = {}
+    mockActivedRoute = {}
+    mockRouter = {}
+    TestBed.configureTestingModule({
+      declarations: [ LogoutComponent ],
+      providers: [
+        { provide: AuthorizeService, useValue: mockAuthService },
+        { provide: ActivatedRoute, useValue: mockActivedRoute },
+        { provide: Router, useValue: mockRouter },
+      ]
+    })
+    .compileComponents();
+  }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(LogoutComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LogoutComponent);
+    component = fixture.componentInstance;
+    authorizeService = fixture.debugElement.injector.get(AuthorizeService);
+    activatedRoute = fixture.debugElement.injector.get(ActivatedRoute);
+    router = fixture.debugElement.injector.get(Router);
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
