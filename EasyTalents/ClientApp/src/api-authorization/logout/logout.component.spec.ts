a063a2bf84db122e79c30490d8b5a72e305f2033
alanduarte90 <alanduarte90@gmail.com>
2020-11-20 17:13:26 -0300

    fix(test): add missing implementation implement mockRouter and mockActivatedRoute in logout component test

--- EasyTalents/ClientApp/src/api-authorization/logout/logout.component.spec.ts
+++ EasyTalents/ClientApp/src/api-authorization/logout/logout.component.spec.ts
@@ -1,9 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActivatedRoute, Router } from '@angular/router';
-import { AuthorizeService } from '../authorize.service';
+import { AuthenticationResultStatus, AuthorizeService } from '../authorize.service';
 import { mockRouteSnapshotLogout } from '../../app/mock-data';
 
 import { LogoutComponent } from './logout.component';
+import { of } from 'rxjs';
 
 describe('LogoutComponent', () => {
   let component: LogoutComponent;
@@ -16,11 +17,23 @@ describe('LogoutComponent', () => {
   let router: Router;
 
   beforeEach(async(() => {
-    mockAuthService = {}
+    mockAuthService = {
+      isAuthenticated: jasmine.createSpy('isAuthenticated').and.returnValue(of(true)),
+      signOut: jasmine.createSpy('signOut').and
+      .returnValue({ status: AuthenticationResultStatus.Success, message: 'fake message' }),
+      completeSignOut: jasmine.createSpy('completeSignOut').and
+      .returnValue({
+        status: AuthenticationResultStatus.Success,
+        state: 'mock',
+        message: 'fake message' })
+    }
     mockActivedRoute = {
       snapshot: mockRouteSnapshotLogout
     }
-    mockRouter = {}
+    mockRouter = {
+      navigate: jasmine.createSpy('navigate').and.callFake(() => {}),
+      navigateByUrl: jasmine.createSpy('navigateByUrl').and.callFake(() => {}),
+    }
     TestBed.configureTestingModule({
       declarations: [ LogoutComponent ],
       providers: [
